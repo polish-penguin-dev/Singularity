bfabc6fdc507d12bcffbcc6639773e5aa21bc153
penguin dev <74113025+polish-penguin-dev@users.noreply.github.com>
2023-10-06 19:30:21 +0100

    Fix some stuff

--- Test/index.js
+++ Test/index.js
@@ -1,8 +1,8 @@
 const { Client, Events } = require("../index");
-const dotenv = require("dotenv");
-dotenv.config();
+//const dotenv = require("dotenv");
+//dotenv.config()
 
-const client = new Client({ token: process.env.TOKEN, intents: 33409 });
+const client = new Client({ token: "TOKEN", intents: 33409 });
 
 client.on("READY", (e) => {
   console.log("The bot is ready!");
@@ -10,9 +10,12 @@ client.on("READY", (e) => {
 
 client.on(Events.messageCreate, async (msg) => {
   if (msg.content === ".join") {
-    await client.voice.joinVoiceChannel(msg.guild_id, "1158494798483828819");
+    await client.voice.joinVoiceChannel(msg.guild_id, "1153384036560031747");
+
+    
     await client.voice.playAudioFile("./Music.mp3");
   }
 });
 
 client.login();
+
